b350d9776539ab4c26acdb4da7a52561e60f363a
YG <710431075@qq.com>
2023-01-01 02:02:59 +0800

    SpringBoot + MyBatisPlus 启动类

--- itcast-mp-springboot/src/main/java/cn/itcast/mp/MyApplication.java
+++ itcast-mp-springboot/src/main/java/cn/itcast/mp/MyApplication.java
@@ -9,7 +9,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("cn.itcast.mp.mapper")
 @SpringBootApplication
 public class MyApplication {
     public static void main(String[] args) {
